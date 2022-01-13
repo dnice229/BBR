9d39cfd9d9f54c2a49e56c48cb99530d1e8ccc55
Denise <amerley229@gmail.com>
2022-01-13 10:38:38 +0100

    updated

--- dag1_startcode/modules/main.py
+++ dag1_startcode/modules/main.py
@@ -11,16 +11,19 @@ class main:
         self.globals.speechProxy.say("walking")
         self.globals.motProxy.moveTo(1, 0, 0)
         self.globals.posProxy.goToPosture('StandInit', 1.0)
+
         # Square
         for i in range(3):
             self.globals.speechProxy.say("turning now")
-            self.globals.motProxy.moveTo(0, 0, 0.785)
+            self.globals.motProxy.moveTo(0, 0, 1.57)
             self.globals.posProxy.goToPosture('StandInit', 1.0)
             self.globals.speechProxy.say("walking")
             self.globals.motProxy.moveTo(1, 0, 0)
             self.globals.posProxy.goToPosture('StandInit', 1.0)
 
-
-
+        # Circle
+        self.globals.motProxy.moveTo(1, 0, 6.24)
+        # End sequence
         self.globals.posProxy.goToPosture('SitRelax', 1.0)
         self.globals.speechProxy.say("Hasta la vista baby")
+
