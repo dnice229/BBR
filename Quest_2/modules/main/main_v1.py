f99fe8c67ee9d27692931b52d67a83867cd854d9
jessefontaine <jesse.fontaine98@gmail.com>
2022-01-25 16:45:34 +0100

    ll

--- Quest_2/modules/main/main_v1.py
+++ Quest_2/modules/main/main_v1.py
@@ -3,10 +3,7 @@ import numpy as np
 from naoqi import ALProxy
 import time
 import sys
-# red_low = [8, 50, 169]
-# red_high= [58, 100, 219]
-# green_low = [7, 86, 19]
-# green_high= [57, 136,69]
+
 class main_v1:
     def setDependencies(self, modules):
         self.globals = modules.getModule("globals")
@@ -14,28 +11,24 @@ class main_v1:
         self.tools = modules.getModule("tools")
         self.vision = modules.getModule("vision")
         self.sonar = modules.getModule("sonar")
-        # self.behaviour = modules.getModule("behaviour")
+        self.behaviour = modules.getModule("behaviour")
 
     def start(self):
         self.globals.setProxies()
         self.motion.init()
         self.tools.cSubscribe()
         # self.globals.posProxy.goToPosture('Stand', 1.0)
-        
 
-        #
         blobsFound = 0
 
-        while blobsFound < 10:
+        while blobsFound < 3:
             raw_img, pos = self.tools.getSnapshot()
             self.tools.SaveImage('1_raw_img.jpg', raw_img)
             cut_img = self.vision.findSquare(raw_img)
             self.tools.SaveImage('2_cut_img.jpg', cut_img)
-            blobsFound, blobList, circles = self.vision.getBlobsData(cut_img)           
+            blobsFound, blobList, circles = self.vision.getBlobsData(cut_img)                      
             self.tools.SaveImage('3_blob_img.jpg', sum(circles))
 
-
-            print(blobsFound)
 
         blobDist = self.vision.calcAvgBlobDistance(blobList)
         # if blobDist is None:
@@ -46,7 +39,6 @@ class main_v1:
         #     blobsFound, blobList, circles = self.vision.getBlobsData(img)
         #     self.globals.motProxy.moveTo(0.20,0,0)
         #     blobDist = self.vision.calcAvgBlobDistance(blobList)
-        print(blobDist)
         center = self.vision.calcMidLandmark(blobList)
         angle = self.vision.calcAngleLandmark(blobList)
         signature  = self.vision.findSignature(blobList)
